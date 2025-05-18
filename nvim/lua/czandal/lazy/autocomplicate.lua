0aeac45b257bcf37e866f363554ea5f9f9695a8c
Radosław Malec <malec.radoslaw@o2.pl>
2025-05-18 18:16:53 +0200

    Enable logging to file for autocomplicate

--- nvim/lua/czandal/lazy/autocomplicate.lua
+++ nvim/lua/czandal/lazy/autocomplicate.lua
@@ -5,16 +5,19 @@ return {
         require("autocomplicate").setup({
             register_autocmd = true,
             default_keymaps = true,
-            model = "qwen2.5-coder:1.5b",
-            -- host= "http://localhost:11445",
-            context_line_size = 5,
+            model = "deepseek-coder-v2:latest",
+            host= "http://localhost:11445",
+            context_line_size = 250,
             llm_options = {
-                num_predict = 10,
+                num_predict = 15,
                 temperature = 0.7,
                 top_k = 30,
                 top_p = 0.8,
-
+                stop = "\n"
             },
+            enable_logging = true,
+            log_to_console = false,
+            path_to_log = "~/CodeShit/dev-setup/autocomplicate.log"
         })
     end
 }
