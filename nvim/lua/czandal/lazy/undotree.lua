471cea1bcc582c2a0e5b77cb46ebfcd6347ee0b6
Radosław Malec <malec.radoslaw@o2.pl>
2025-07-17 10:47:19 +0200

    Fix undotree remap after undotree update

--- nvim/lua/czandal/lazy/undotree.lua
+++ nvim/lua/czandal/lazy/undotree.lua
@@ -2,7 +2,7 @@ return {
     "mbbill/undotree",
 
     config = function()
-        vim.keymap.set("n", "<leader>u", vim.cmd.undotreetoggle)
+        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
     end
 }
 
