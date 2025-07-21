f267e585b88ba038f21ca222ad30603c016933f9
Radosław Malec <malec.radoslaw@o2.pl>
2025-07-21 14:44:55 +0200

    Show hidden files in telescope files search

--- nvim/lua/czandal/lazy/telescope.lua
+++ nvim/lua/czandal/lazy/telescope.lua
@@ -12,7 +12,9 @@ return {
 
         local builtin = require('telescope.builtin')
         -- general find
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>pf', function()
+            builtin.find_files({ hidden = true })
+        end)
         -- find next word
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
